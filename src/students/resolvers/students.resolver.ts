bf4816018317c4ff9c405669f71f02e506718bd0
Sabine <sabine.bertram@mailbox.org>
2020-09-11 16:54:22 +0200

    fix: remove unused service

--- src/students/resolvers/students.resolver.ts
+++ src/students/resolvers/students.resolver.ts
@@ -4,14 +4,10 @@ import { Student } from '../models/student.model';
 import { CurrentUser } from 'src/auth/current-user.decorator';
 import { User } from 'src/common/interfaces/user.interface';
 import { StudentEventQuery } from '../models/student-event-query.model';
-import { CustomersService } from 'src/customers/customers.service';
 
 @Resolver(_of => Student)
 export class StudentsResolver {
-  constructor(
-    private readonly studentsService: StudentsService,
-    private readonly customersService: CustomersService,
-  ) {}
+  constructor(private readonly studentsService: StudentsService) {}
 
   @Query(_returns => Student)
   async getStudent(@CurrentUser() user: User): Promise<Student> {
