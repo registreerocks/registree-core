7b4fb931c48930a351b4bd49d5f44185b047ec34
Pi Delport <pi@registree.io>
2021-01-20 15:57:41 +0200

    refactor(auth): use userFromGqlContext helper inside @CurrentUser

--- src/auth/current-user.decorator.ts
+++ src/auth/current-user.decorator.ts
@@ -1,22 +1,11 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
-import { IncomingMessage } from 'http';
 import { User } from 'src/common/interfaces/user.interface';
+import { userFromGqlContext } from '../common/graphql.helpers';
 
 export const CurrentUser = createParamDecorator(
   (data: unknown, context: ExecutionContext): User => {
     const ctx = GqlExecutionContext.create(context);
-
-    const user = ctx.getContext<{
-      req: IncomingMessage & { user?: User };
-    }>().req.user;
-
-    if (user) {
-      return user;
-    } else {
-      throw new Error(
-        'User parameter requested when no user were identified on the request. Ensure that the authentication middleware ran for the request.',
-      );
-    }
+    return userFromGqlContext(ctx.getContext());
   },
 );
