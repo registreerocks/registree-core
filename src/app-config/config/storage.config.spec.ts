b0d9674f9851d2a0d57b97ed9080aab1c366e116
Pi Delport <pi@registree.io>
2021-01-22 20:45:04 +0200

    test(config): add initial tests for config accessors
    These are mainly to capture the current behaviour, before making any
    major changes.

---
+++ src/app-config/config/storage.config.spec.ts
@@ -0,0 +1,83 @@
+import * as dotenv from 'dotenv';
+import * as fc from 'fast-check';
+import * as fs from 'fs';
+import { hasAllKeys, withMockedEnv } from '../../common/test.helpers';
+import { StorageConfig } from './storage.config';
+
+describe('StorageConfig', () => {
+  test('empty config fails', () => {
+    expect(() => withMockedEnv({}, StorageConfig)).toThrow(
+      new Error('Incomplete storage config provided'),
+    );
+  });
+
+  test('invalid config fails', () => {
+    const invalidEnv = fc
+      .record(
+        {
+          LOCAL_OBJECT_STORAGE: fc.string().filter(s => s !== 'true'),
+          S3_ENDPOINT: fc.string(),
+          S3_ACCESS_KEY_ID: fc.string(),
+          S3_SECRET: fc.string(),
+        },
+        { requiredKeys: [] },
+      )
+      .filter(
+        env =>
+          !hasAllKeys(env, ['S3_ENDPOINT', 'S3_ACCESS_KEY_ID', 'S3_SECRET']),
+      );
+
+    fc.assert(
+      fc.property(invalidEnv, env => {
+        expect(() => withMockedEnv(env, StorageConfig)).toThrow(
+          new Error('Incomplete storage config provided'),
+        );
+      }),
+    );
+  });
+
+  test('valid config', () => {
+    const validEnvLocal = fc.record(
+      {
+        LOCAL_OBJECT_STORAGE: fc.constant('true'),
+        S3_ENDPOINT: fc.oneof(fc.constant(''), fc.domain()),
+        S3_ACCESS_KEY_ID: fc.string(),
+        S3_SECRET: fc.string(),
+      },
+      { requiredKeys: ['LOCAL_OBJECT_STORAGE'] },
+    );
+    const validEnvNonLocal = fc.record(
+      {
+        LOCAL_OBJECT_STORAGE: fc.string().filter(s => s !== 'true'),
+        S3_ENDPOINT: fc.domain(),
+        S3_ACCESS_KEY_ID: fc.string({ minLength: 1 }),
+        S3_SECRET: fc.string({ minLength: 1 }),
+      },
+      { requiredKeys: ['S3_ENDPOINT', 'S3_ACCESS_KEY_ID', 'S3_SECRET'] },
+    );
+    const validEnv = fc.oneof(validEnvLocal, validEnvNonLocal);
+
+    fc.assert(
+      fc.property(validEnv, env => {
+        expect(withMockedEnv(env, StorageConfig)).toStrictEqual({
+          useLocal: env.LOCAL_OBJECT_STORAGE === 'true',
+          endpoint: env.S3_ENDPOINT,
+          accessKeyId: env.S3_ACCESS_KEY_ID,
+          secretAccessKey: env.S3_SECRET,
+        });
+      }),
+    );
+  });
+
+  test('example config snapshot', () => {
+    const exampleEnv = dotenv.parse(fs.readFileSync('.env.example'));
+    expect(withMockedEnv(exampleEnv, StorageConfig)).toMatchInlineSnapshot(`
+      Object {
+        "accessKeyId": undefined,
+        "endpoint": undefined,
+        "secretAccessKey": undefined,
+        "useLocal": true,
+      }
+    `);
+  });
+});
