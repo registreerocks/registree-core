e70e78bc3c8b1693f29df7af5715f79f08afbdd1
Pi Delport <pi@registree.io>
2021-02-03 20:53:25 +0200

    test(queries): add fast-check generators and tests for CreateEventQueryInput

---
+++ src/queries/dto/create-event-query.input.spec.ts
@@ -0,0 +1,46 @@
+import fc from 'fast-check';
+import { validateToErrorMessage } from '../../common/test.helpers';
+import { CreateEventQueryInput } from './create-event-query.input';
+import {
+  arbitraryCreateEventQueryInput,
+  arbitraryInvalidCreateEventQueryInput,
+} from './create-event-query.input.arb';
+
+describe('CreateEventQueryInput', () => {
+  test('empty input fails', async () => {
+    expect(await validateToErrorMessage(new CreateEventQueryInput()))
+      .toMatchInlineSnapshot(`
+      "An instance of CreateEventQueryInput has failed the validation:
+       - property name has failed the following constraints: minLength 
+      An instance of CreateEventQueryInput has failed the validation:
+       - property address has failed the following constraints: minLength 
+      An instance of CreateEventQueryInput has failed the validation:
+       - property startDate has failed the following constraints: isDate 
+      An instance of CreateEventQueryInput has failed the validation:
+       - property endDate has failed the following constraints: isDate 
+      An instance of CreateEventQueryInput has failed the validation:
+       - property information has failed the following constraints: minLength 
+      An instance of CreateEventQueryInput has failed the validation:
+       - property degrees has failed the following constraints: isArray 
+      An instance of CreateEventQueryInput has failed the validation:
+       - property eventType has failed the following constraints: minLength 
+      "
+    `);
+  });
+
+  test('arbitrary valid input', async () => {
+    await fc.assert(
+      fc.asyncProperty(arbitraryCreateEventQueryInput(), async input => {
+        expect(await validateToErrorMessage(input)).toBe('');
+      }),
+    );
+  });
+
+  test('arbitrary invalid input', async () => {
+    await fc.assert(
+      fc.asyncProperty(arbitraryInvalidCreateEventQueryInput(), async input => {
+        expect(await validateToErrorMessage(input)).not.toBe('');
+      }),
+    );
+  });
+});
